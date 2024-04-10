72f7fcea35a8877493dcfa414b50bfe76320a4c6
FrnBrz <frnbrz@gmail.com>
2024-04-10 15:40:16 +0200

    Add new app-page component and related files

---
+++ src/app/pages/app-page/app-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-app-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './app-page.component.html',
+  styleUrls: ['./app-page.component.scss']
+})
+export class AppPageComponent {
+
+}
